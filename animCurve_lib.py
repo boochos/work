4e5d94397ccf854add7b31b49ae3c997878ab867
boochos <sebastianweber@live.com>
2013-11-25 12:15:17 -0800

    Update animCurve_lib.py
    copied from dd

--- animCurve_lib.py
+++ animCurve_lib.py
@@ -10,6 +10,67 @@ class GetRange():
         self.max = cmds.playbackOptions(q=True, maxTime=True)
         self.current = cmds.currentTime(q=True)
 
+'''
+import animCurve_lib as ac
+reload(ac)
+objects = cmds.ls(sl=1)
+start = cmds.playbackOptions(q=1,min=1)
+end = cmds.playbackOptions(q=1,max=1)
+bakeTimeWarp(objects,start,end,killWarp=True)
+'''
+
+def bakeTimeWarp(objects,start,end,killWarp=True):
+    # for each frame between start and end, query time1.outTime and time1.unwarpedTime
+    # for each object, get each channel with at least one keyframe set
+    # for each channel:
+    #     get the value of the channel at outTime
+    #     set the channel to this value at unwarpedTime and set a keyframe
+    for i in objects:
+        dupe = cmds.duplicate(i,po=1)[0]
+        dupe = cmds.rename(dupe, dupe + '__warped')
+        if not cmds.attributeQuery('bakeTimeWarpConnection',node=i,ex=1):
+            cmds.addAttr(i,ln='bakeTimeWarpConnection',at='message')
+        cmds.connectAttr(dupe+'.message',i+'.bakeTimeWarpConnection')
+    for x in range(start,end+1):
+        cmds.currentTime(x)
+        outTime = cmds.getAttr('time1.outTime')
+        unwarpedTime = cmds.getAttr('time1.unwarpedTime')
+        for i in objects:
+            # build a list of all keyed channels.
+            keyables = cmds.listAttr(i,k=1)
+            keyedChans = []
+            for f in keyables:
+                if cmds.keyframe(i+'.'+f,q=1,n=1):
+                    try:
+                        keyedChans.append(f)
+                    except:
+                        print f, '  skipped\n'
+            dupe = cmds.listConnections(i+'.bakeTimeWarpConnection')[0]
+            for chan in keyedChans:
+                val = cmds.getAttr(i+'.'+chan,t=outTime)
+                cmds.setAttr(dupe+'.'+chan,val)
+                cmds.setKeyframe(dupe+'.'+chan,t=unwarpedTime)
+    # now reconnect anim curves from the duplicate to the original. then delete the duplicates and finally remove the timewarp.
+    for i in objects:
+        dupe = cmds.listConnections(i+'.bakeTimeWarpConnection')[0]
+        chans = [f for f in cmds.listAttr(dupe,k=1) if cmds.keyframe(dupe+'.'+f,q=1,n=1)]
+        for chan in chans:
+            animCurve = cmds.keyframe(dupe+'.'+chan,q=1,n=1)[0]
+            oldCurve = cmds.keyframe(i+'.'+chan,q=1,n=1)
+            cmds.connectAttr(animCurve+'.output',i+'.'+chan,f=1)
+            cmds.delete(oldCurve)
+        cmds.delete(dupe)
+        cmds.deleteAttr(i+'.bakeTimeWarpConnection')
+    if killWarp:
+        timeWarp = cmds.listConnections('time1.timewarpIn_Raw')[0]
+        cmds.delete(timeWarp)
+
+def animScale():
+    scale_factor = 4.0389
+    scale_pivot  = cmds.playbackOptions(q=True, min=True)
+    cmds.scaleKey(ts=scale_factor, tp=scale_pivot)
+
+
 def scaleCrv(val):
     '''
     -Scale selected Graph Editor curves with given value.
