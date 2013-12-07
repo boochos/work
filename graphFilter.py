fbfa5055877d453e7c1c803607cae249d6331701
boochos <sebastianweber@live.com>
2013-12-07 15:25:52 -0800

    Update graphFilter.py
    removed some commenting

--- graphFilter.py
+++ graphFilter.py
@@ -5,39 +5,30 @@ def graphFilterCore(attr='', panel=''):
     '''\n
     adds/clears filters for the default graphEditor
     '''
-    #graph editor (default graphEditor supported only)
-    #panel = cmds.getPanel(wf=True) #old way,
     #check for any filters currently being used
-    c = cmds.outlinerEditor(panel + 'OutlineEd', q=True, af=True)
+    c  = cmds.outlinerEditor(panel + 'OutlineEd', q=True, af=True)
+    cf = None
     #if attrs arg is not empty execute, else delete filters reset filter to None
     if attr != '':
         #make new filter
         f = cmds.itemFilterAttr(bn=attr)
-        #f = cmds.rename(f, panel + '_attrFilter___' + attr.replace('*', '_') + '___')
         #if a filter is already being used merge new/current #else use new filter
         if c != '0':
             #unify new/current filter
             cf = cmds.itemFilterAttr( un=[c, f])
-            #delete current filter
             cmds.delete(c)
             #set unified filter
-            ##for graph in panel:
             cmds.outlinerEditor(panel + 'OutlineEd', e=True, af=cf)
         else:
             #set new filter
-            ##for graph in panel:
             cmds.outlinerEditor(panel + 'OutlineEd', e=True, af=f)
     else:
         if c != '0':
             #clear all user filters
-            #d = cmds.itemFilterAttr( luf=True)
-            #cmds.delete(d)
             cmds.delete(c)
         #reset filters to None
-        ##for graph in panel:
         cmds.outlinerEditor(panel + 'OutlineEd', e=True, af=0)
-        #print '---filters for ' + panel + ' cleared---'
-    
+
 def graphFilters(attrs, panel=''):
     '''\n
     attrs should be seperated by commas
@@ -45,7 +36,6 @@ def graphFilters(attrs, panel=''):
     '''
     shortName = ['tx', 'ty', 'tz', 'rx', 'ry', 'rz', 'sx', 'sy', 'sz']
     longName = ['translateX', 'translateY', 'translateZ', 'rotateX', 'rotateY', 'rotateZ', 'scaleX', 'scaleY', 'scaleZ']
-    print panel, '___panel'
     if attrs == '':
         graphFilterCore(attr='',panel=panel)
     else:
