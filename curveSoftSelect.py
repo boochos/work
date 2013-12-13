5b4a2e4803fdddb05862f5f3098e671d2de1d0a1
boochos <sebastianweber@live.com>
2013-12-13 12:43:24 -0800

    Update curveSoftSelect.py
    works... lattice still better

--- curveSoftSelect.py
+++ curveSoftSelect.py
@@ -38,35 +38,54 @@ def jobValue(*args):
             c = 0
             for crv in lcCrv:
                 frames     = cmds.keyframe(crv, q=True, sl=True)
+                drop = len(frames)
+                if drop == 1:
+                    func = [0,1]
+                else:
+                    func = [2,2]
                 startFrame = frames[0]
                 endFrame   = frames[len(frames)-1]
                 frames     = [startFrame, endFrame]
-                print '__________________________________________________________________'
-                print frames, '___frames'
+                #print '__________________________________________________________________'
+                #print frames, '___frames'
                 #iterators for correct list positions in global vars
                 v = 0
                 side = ['l', 'r']
                 for frame in frames:
-                    print frame, '__ left right frame ittr'
+                    #print frame, '__ left right frame ittr'
                     lcVal  = cmds.keyframe(crv, q=True, vc=True, time=(frame, frame))[0]
                     glval  = glVal[c][v]
                     if glval != lcVal:
-                        print glval, '__global ', lcVal, '__local '
+                        #print glval, '__global ', lcVal, '__local '
                         offset = glval - lcVal
                         if offset < 0:
                             offset = offset*-1
-                        framesBlend = neibors(crv=crv, splitFrame=frame, drop=5, direction=side[v])
+                        framesBlend = neibors(crv=crv, splitFrame=frame, drop=drop, direction=side[v])
                         #needs to be start at 1, 0 is the first position, full weight
                         b = 1
                         for f in framesBlend:
-                            print f, '__blend frame ittr'
+                            #print f, '__blend frame ittr'
                             if f != frame:
                                 if v == 0:
-                                    blnd = blendWeight2(framesBlend, b)[1]
-                                    print blnd, '____start'
+                                    #left side of plsit frame
+                                    #do math to figure out frame position in drop range
+                                    pos = frame - f
+                                    #flip value so blendweight gets correct input
+                                    pos = drop - pos + 1
+                                    #print pos
+                                    #get blend weight value with position number
+                                    #blnd = blendWeight2(drop, b)[1]
+                                    blnd = blendWeight2(drop, pos, func[0])[1]
+                                    #print blnd, '____start'
                                 else:
-                                    blnd = blendWeight2(framesBlend, b)[0]
-                                    print blnd, '____end'
+                                    #do math to figure out frame position in drop range
+                                    pos = f - frame
+                                    #print pos
+                                    #get blend weight value with position number
+                                    #right side of plsit frame
+                                    #blnd = blendWeight2(drop, b)[0]
+                                    blnd = blendWeight2(drop, pos, func[1])[0]
+                                    #print blnd, '____end'
                                 val  = cmds.keyframe(crv, q=True, vc=True, time=(f, f))[0]
                                 if glval < lcVal:
                                     cmds.keyframe(crv, vc=val+(offset*blnd), time=(f, f))
@@ -78,7 +97,8 @@ def jobValue(*args):
                         message('did math')
                         message('\n\n')
                     else:
-                        print '________________________value variable is the same'
+                        pass
+                        #print '________________________value variable is the same'
                     v=v+1
                 c=c+1
         else:
@@ -91,7 +111,7 @@ def jobUndo(*args):
 
 def killUndoJob(*args):
     global idUndo
-    print idUndo, '___killing undo job'
+    #print idUndo, '___killing undo job'
     getJobs = cmds.scriptJob(lj=True)
     #print getJobs
     jobs = []
@@ -109,12 +129,12 @@ def killUndoJob(*args):
 def activateUndoJob(*args):
     global idUndo
     idUndo = cmds.scriptJob( e= ["Undo", "import curveSoftSelect as css\ncss.jobUndo()"])
-    print idUndo
+    #print idUndo
     message('Undo script ON', maya=True)
 
 def killValueJob(*args):
     global idV
-    print idV, '___killing value job'
+    #print idV, '___killing value job'
     getJobs = cmds.scriptJob(lj=True)
     #print getJobs
     jobs = []
@@ -132,7 +152,7 @@ def killValueJob(*args):
 
 def activateValueJob(*args):
     global idV
-    print idV, '___activate'
+    #print idV, '___activate'
     #print idV
     if plug():
         idV = cmds.scriptJob( ac= [plug(), "import curveSoftSelect as css\ncss.jobValue()"])
@@ -149,10 +169,10 @@ def jobSel(*args):
     if lcCrv:
         killValueJob()
         activateValueJob()
-        print 'activate value'
+        #print 'activate value'
     else:
         killValueJob()
-        print 'killed value'
+        #print 'killed value'
 
 def killSelJob(*args):
     getJobs = cmds.scriptJob(lj=True)
@@ -235,35 +255,22 @@ def neibors(crv='', splitFrame=0.0, drop=1, direction='l'):
     drop should consider frame boundaries and find keys within, not keyframes on either side
     '''
     frames = cmds.keyframe(crv, q=True)
-    start  = []
-    end    = []
     result = []
     if direction == 'l':
-        dropoff = splitFrame - drop, '___dropoff'
+        dropoff = splitFrame - drop
     else:
-        dropoff = splitFrame + drop, '___dropoff'
-    print dropoff
+        dropoff = splitFrame + drop
     i = 0
     for frame in frames:
-        if frame >= dropoff:
-            print frame, '___________qualifies'
-        if frame == splitFrame:
-            if direction == 'l':
-                start = frames[i-drop:i]
-                for f in start:
-                    result.append(f)
-                return result
-            else:
-                end   = frames[i+1:i+drop+1]
-                for f in end:
-                    result.append(f)
-                print result
-                return result
-            #result.append(splitFrame)
+        if direction == 'l':
+            if frame >= dropoff and frame < splitFrame:
+                result.append(frame)
         else:
-            i = i + 1
+            if frame <= dropoff and frame > splitFrame:
+                result.append(frame)
+    return result
 
-def blendWeight2(List, i, function=2, falloff=0.0):
+def blendWeight2(numOfPoints=1, i=1, function=2, falloff=0.0):
     '''
     Arguments:\n
         List = list of objects
@@ -287,7 +294,7 @@ def blendWeight2(List, i, function=2, falloff=0.0):
     #orig
     #numOfPoints = len(List)-1
     #new
-    numOfPoints = len(List)+1
+    numOfPoints = numOfPoints+1
 
 
     if function == 0:
@@ -300,8 +307,8 @@ def blendWeight2(List, i, function=2, falloff=0.0):
         ##weight[0] = math.cos(math.radians(iDegree*i))
         weight[0] = math.cos(math.radians(iDegree*i))
         weight[1] = 1 - weight[0]
-        weight[1] =  weight[1]/math.exp(decay*(len(List) -(i)))
-        weight[0] = 1 - weight[1]
+        #weight[1] =  weight[1]/math.exp(decay*(len(List) -(i)))
+        #weight[0] = 1 - weight[1]
         return weight
     elif function == 1:
         #Ease In
@@ -313,8 +320,8 @@ def blendWeight2(List, i, function=2, falloff=0.0):
         ##weight[1] = math.sin(math.radians(iDegree*i))
         weight[1] = math.sin(math.radians(iDegree*i))
         weight[0] = 1 - weight[1]
-        weight[0] = weight[0]/math.exp(decay*(i+1))
-        weight[1] = 1 - weight[0]
+        #weight[0] = weight[0]/math.exp(decay*(i+1))
+        #weight[1] = 1 - weight[0]
         return weight
     elif function == 2:
         #Ease Out Ease In
