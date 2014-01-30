9ccf7e97e6b8452021bf34821baf663919f053f0
boochos <sebastianweber@live.com>
2014-01-29 23:43:06 -0800

    minor

--- control_lib.py
+++ control_lib.py
@@ -3,104 +3,15 @@ import maya.cmds as cmds
 import maya.OpenMaya as OpenMaya
 import atom_miscellaneous_lib as misc
 
-def setPreset(*args):
-    idx = cmds.optionMenu('atom_qls_limb_preset_optionMenu', query=True, sl=True )
-    flipCheck = 'atom_qls_flip_checkBoxGrp'
-    pvFlip    = 'atom_qls_pvFlip_checkBoxGrp'
-    ldf       = 'atom_qls_ldf_floatField'
-    pldf      = 'atom_paw_qls_ldf_floatField'
-    suffix    = 'atom_suffix_optionMenu'
-    #back left leg
-    if idx ==  1:
-        cmds.floatFieldGrp('atom_qls_anklePvFlip_floatFieldGrp', edit=True,v1=0,v2=5,v3=5)
-        cmds.optionMenu(suffix, edit=True, sl = 1)
-        cmds.checkBoxGrp(flipCheck, edit=True, va3=[False,False,False])
-        cmds.checkBoxGrp(pvFlip,    edit=True, va3=[False,False,False])
-        cmds.floatField(ldf,        edit=True, v=10.0)
-        cmds.floatField(pldf,       edit=True, v=3.25)
-    
-    #back right leg
-    elif idx == 2:
-        cmds.floatFieldGrp('atom_qls_anklePvFlip_floatFieldGrp', edit=True,v1=0,v2=-5,v3=-5)
-        cmds.optionMenu(suffix, edit=True, sl = 2)
-        cmds.checkBoxGrp(flipCheck, edit=True, va3=[False,True,True])
-        cmds.checkBoxGrp(pvFlip,    edit=True, va3=[False,False,False])
-        cmds.floatField(ldf,        edit=True, v=-10.0)
-        cmds.floatField(pldf,       edit=True, v=-3.25)
-    
-    #front left leg
-    elif idx == 3:
-        cmds.floatFieldGrp('atom_qls_anklePvFlip_floatFieldGrp', edit=True,v1=0,v2=5,v3=5)
-
-        
-        cmds.optionMenu(suffix, edit=True, sl = 1)
-        cmds.checkBoxGrp(flipCheck, edit=True, va3=[False,False,False])
-        cmds.checkBoxGrp(pvFlip,    edit=True, va3=[True,False,False])
-        cmds.floatField(ldf,        edit=True, v=-10.0)
-        cmds.floatField(pldf,       edit=True, v=3.25)
-
-    #front right leg
-    elif idx == 4:
-        cmds.floatFieldGrp('atom_qls_anklePvFlip_floatFieldGrp', edit=True,v1=0,v2=-5,v3=-5)
-        cmds.optionMenu(suffix, edit=True, sl = 2)
-        cmds.checkBoxGrp(flipCheck, edit=True, va3=[False,True,True])
-        cmds.checkBoxGrp(pvFlip,    edit=True, va3=[True,False,False])
-        cmds.floatField(ldf,        edit=True, v=10.0)
-        cmds.floatField(pldf,       edit=True, v=-3.25)
-        
-#-------------
-#Name        :validateFieldTextInput
-#Arguements  :<control> : textFieldGrp
-#Description :builds a name based on the selected items in the Daily Maker windows optionMenuGrp's
-#-------------
-def validateFieldTextInput(control):
-    returnStr = cmds.textField(control, query=True, tx=True)
-    exp     =' !@#$%^&*()+-={}|[]\\:\"\';<>?,./'
-    if len(returnStr) > 0:
-        for element in returnStr:
-            for itm in exp:
-                if element == itm:
-                    cmds.textField(control, edit = True, tx = 'None')
-                    returnStr = 'None'
-                    break
-    else:
-        cmds.textField(control, edit = True, tx = 'None')
-#end def validateFieldTextInput
-
-#-------------
-#Name        :refreshWindow
-#Arguements  :<control> : window
-#Description :Change the size of the window by one pixel, then back to its original size.
-#             This forces a refresh on the windows internal controls
-#-------------
-def refreshWindow(control):
-    currentSize = cmds.window(control, query=True, width=True)
-    cmds.window(control, edit=True, width = currentSize + 1)
-    cmds.window(control, edit=True, width = currentSize)
-#end def refreshWindow
-
-#-------------
-#Name        :addControlCurveButton
-#Arguements  :<path>: str
-#Description :Builds a button that called atom_ui_lib.imortCurveShape to shape pre-defined curves
-#Notes       :Used in the Control Curve Shape Toolbox section of the Atom win 
-#-------------
-def addControlCurveButton(path):
-    files =  os.listdir(path)
-    files.sort()
-    for file in files:
-        txtSplit = file.split('.')
-        if txtSplit[1] == 'txt':
-            cmds.button(label=txtSplit[0], c='from atom import atom_ui_lib\natom_ui_lib.importCurveShape("'+txtSplit[0]+'","'+path +'")')
-#end def addControlCurveButton
-
-#-------------
-#Name        :exportCurveShape
-#Arguements  :N/A
-#Description :Exports the local transforms of the cvs on the selected curve.
-#Notes       :This is designed to only work with the Atom win 
-#-------------
 def exportCurveShape(*args):
+    '''
+    #-------------
+    #Name        :exportCurveShape
+    #Arguements  :N/A
+    #Description :Exports the local transforms of the cvs on the selected curve.
+    #Notes       :This is designed to only work with the Atom win 
+    #-------------
+    '''
     #get the selection
     sel = cmds.ls(selection=True)
     if len(sel) == 1:
@@ -125,16 +36,18 @@ def exportCurveShape(*args):
                     refreshWindow('atom_win')
     else:
         print 'Select one curve to export'
-#end def exportCurveShape
 
-#-------------
-#Name        :importCurveShape
-#Arguements  :<name>: str
-#            :<path>: str
-#Description :Imports a curve shape based on the path and name info
-#Notes       :Function expects a .txt file
-#-------------
+
 def importCurveShape(name,path, codeScale = False, overRide = False):
+    '''
+    #-------------
+    #Name        :importCurveShape
+    #Arguements  :<name>: str
+    #            :<path>: str
+    #Description :Imports a curve shape based on the path and name info
+    #Notes       :Function expects a .txt file
+    #-------------
+    '''
     selection  = cmds.ls(selection=True, tr=True)
     path = path + '/' + name + '.txt' 
     #change the shape of multiple selected curves
@@ -172,63 +85,3 @@ def importCurveShape(name,path, codeScale = False, overRide = False):
                     OpenMaya.MGlobal.displayError('CV count['+str(len(cmds.getAttr(shapeNode + '.cv[*]')))+'] from selected does not match import CV count['+str(len(cvInfo))+']')
     else:
         OpenMaya.MGlobal.displayError('Select a NURBS curve if you truly want to proceed...')
-#end def importCurveShape
-
-#-------------
-#Name        :getRadioSelectionAsList
-#Arguements  :<control>: radioButtonGrp 
-#Description :Queries the radioButtonGrp for which buttons are on returns a 3 element array
-#            :0 is not selected, 1 is selected
-#-------------
-def getRadioSelectionAsList(control):
-    returnList = []
-    sel        = cmds.radioButtonGrp(control,  query=True, sl=True)
-    nrb        = cmds.radioButtonGrp(control,  query=True, nrb=True)
-    for i in range(0,nrb,1):
-        if i+1 == sel:
-            returnList.append(1)
-        else:
-            returnList.append(0)
-
-    return returnList
-#end def getRadioSelectionAsList
-
-def createListForTransform(control, value):
-
-    valList  = [value,value,value]
-    axisList = getRadioSelectionAsList(control)
-    
-    for i in range(0,len(valList),1):
-        valList[i] = valList[i] * axisList[i]
-    return valList
-
-#-------------
-#Name        :getCheckBoxSelectionAsList
-#Arguements  :<control>: checkBoxGro 
-#Description :Queries the checkBoxGrp for which buttons are on returns a 3 element array
-#            :0 is not selected, 1 is selected
-#Notes       :For some reason maya doesn't want to return this, all though it seems to be
-#             available in the docs
-def getCheckBoxSelectionAsList(control):
-    checked=[]
-    checked.append(cmds.checkBoxGrp(control, query=True, v1=True))
-    checked.append(cmds.checkBoxGrp(control, query=True, v2=True))
-    checked.append(cmds.checkBoxGrp(control, query=True, v3=True))
-    
-    return checked
-#end def getCheckBoxSelectionAsList
-
-#-------------
-#Name        :convertAxisNum
-#Arguements  :<num>: int 
-#Description :Convert, 1,2,3 to X Y or Z
-def convertAxisNum(num):
-    if num ==1 :
-        return 'X'
-    elif num ==2:
-        return 'Y'
-    elif num == 3:
-        return 'Z'
-    else:
-        return None
-#end def convertAxisNum
