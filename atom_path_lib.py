79921a44e2180a79e7963bc54b68b7c8cd19d719
boochos <sebastiaweber@live.com>
2015-05-13 01:03:23 -0700

    path for patricia

--- atom_path_lib.py
+++ atom_path_lib.py
@@ -3,37 +3,40 @@ import atom_miscellaneous_lib as misc
 import atom_placement_lib as place
 import constraint_lib as cn
 
+
 def attach(*args):
-    #select control and path namespace
-    sel  = cmds.ls(sl=True)
-    path = sel[len(sel)-1]
+    # select control and path namespace
+    sel = cmds.ls(sl=True)
+    path = sel[len(sel) - 1]
     path = path.split(':')[0] + ':path'
-    #remove path from list
-    sel.remove(sel[len(sel)-1])
-    i    = 0
-    mlt  = 1.0/len(sel)
-    s    = 0.0
-    e    = 0.3
+    # remove path from list
+    sel.remove(sel[len(sel) - 1])
+    i = 0
+    mlt = 1.0 / len(sel)
+    s = 0.0
+    e = 0.3
     locs = []
     for ct in sel:
         cmds.select(ct)
-        loc  = cn.locatorOnSelection(constrain=False)
+        loc = cn.locatorOnSelection(constrain=False)
         locs.append(loc[0])
-        min  = cmds.playbackOptions(q=True, minTime=True)
-        max  = cmds.playbackOptions(q=True, maxTime=True)
+        min = cmds.playbackOptions(q=True, minTime=True)
+        max = cmds.playbackOptions(q=True, maxTime=True)
         cmds.parentConstraint(loc, ct, mo=True)
-        #add world up object to path
-        cmds.pathAnimation( loc, c=path, startTimeU=min, endTimeU=max, startU=s, endU=e, follow=True )
-        i = i+1
-        s = s+mlt
-        e = e+mlt
+        # add world up object to path
+        cmds.pathAnimation(loc, c=path, startTimeU=min, endTimeU=max, startU=s, endU=e, follow=True)
+        i = i + 1
+        s = s + mlt
+        e = e + mlt
     cmds.select(clear=True)
+    grp = cmds.group(name='__PATH_GRP__#', em=True)
+    cmds.parent(locs, grp)
     cmds.select(locs)
 
 
 def path(*args):
 
-    #select existing Joint Chain and create List----------------------------------------------------------------------------------------------------------------------------------------------------------
+    # select existing Joint Chain and create List----------------------------------------------------------------------------------------------------------------------------------------------------------
     '''
     cmds.select ('upperD_jnt_05', hi=True)
     names=cmds.ls(sl=True)
@@ -48,9 +51,9 @@ def path(*args):
     cmds.rename('pathJnt_000','root_jnt')
     newSpine=cmds.ls(sl=True)
     '''
-    #Build Pre-Rig-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    
-    #creates groups and master controller from arguments specified as 'True'
+    # Build Pre-Rig-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    # creates groups and master controller from arguments specified as 'True'
 
     misc.rigPrebuild(Top=1, Ctrl=True, SknJnts=False, Geo=False, World=True, Master=True, OlSkool=False, Size=150)
     #misc.cleanUp(newSpine[0], Ctrl=False, SknJnts=True, Body=False, Accessory=False, Utility=False, World=False, olSkool=False)
@@ -84,95 +87,92 @@ def path(*args):
         i=i+1
 
     '''
-    #create path for IkSpline--------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # create path for IkSpline--------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    path='path'
-    cmds.curve(n=path,d=3,p=[(0,0,-1.128),(0,0,-50),(0,0,-100),(0,0,-150),(0,0,-600),])
-    ##cmds.curve(n=path,d=3,p=[(0,0,-607.033),(0,0,-50),(0,0,-100),(0,0,-150),(0,0,-1832.967),])
-    cmds.rebuildCurve(path,d=3,rt=0, s=45)
+    path = 'path'
+    cmds.curve(n=path, d=3, p=[(0, 0, -1.128), (0, 0, -50), (0, 0, -100), (0, 0, -150), (0, 0, -600), ])
+    # cmds.curve(n=path,d=3,p=[(0,0,-607.033),(0,0,-50),(0,0,-100),(0,0,-150),(0,0,-1832.967),])
+    cmds.rebuildCurve(path, d=3, rt=0, s=45)
     #cmds.setAttr(path + '.template', 1)
     misc.cleanUp(path, Ctrl=False, SknJnts=False, Body=False, Accessory=False, Utility=False, World=True, olSkool=False)
 
-    #Place Clusters on CVs, group, and add Controllers---------------------------------------------------------------------------------------------------------------------------------------------
+    # Place Clusters on CVs, group, and add Controllers---------------------------------------------------------------------------------------------------------------------------------------------
 
-    #place Clusters on CVs
-    cl=place.clstrOnCV(path, 'Clstr')
+    # place Clusters on CVs
+    cl = place.clstrOnCV(path, 'Clstr')
 
-
-
-    #place Controls on Clusters and Constrain
+    # place Controls on Clusters and Constrain
     segmentPar = None
     segmentChld = []
-    colors  = [12,15,11]
+    colors = [12, 15, 11]
     color = 0
     colorP = 0
-    i=0
-    j=1
-    k=0
-    v=0
-    Ctrls=[]
+    i = 0
+    j = 1
+    k = 0
+    v = 0
+    Ctrls = []
     for handle in cl:
-        #segment colors
+        # segment colors
         if j < 4:
-            if v==0:
+            if v == 0:
                 color = colors[0]
             else:
                 color = colors[2]
             colorP = color
-            j=j+1
+            j = j + 1
         elif j == 4:
             color = colors[1]
-            j=j+1
+            j = j + 1
         elif j > 4 and j < 8:
-            if v==0:
+            if v == 0:
                 color = colors[0]
             else:
                 color = colors[2]
             colorP = color
-            j=j+1
+            j = j + 1
         elif j == 8:
             color = colors[1]
-            j=1
-            #switch colors
-            if v==0:
-                v=1
+            j = 1
+            # switch colors
+            if v == 0:
+                v = 1
             else:
-                v=0
-        #Controls on Clusters
-        cnt = place.Controller('Point'+ str(('%0' + str(2) + 'd')%(i)), handle, orient = False, shape = 'splineEnd_ctrl', size = 60, color = color, sections = 8, degree = 1, normal =( 0 , 0 , 1), setChannels = True, groups = True)
+                v = 0
+        # Controls on Clusters
+        cnt = place.Controller('Point' + str(('%0' + str(2) + 'd') % (i)), handle, orient=False, shape='splineEnd_ctrl', size=60, color=color, sections=8, degree=1, normal=(0, 0, 1), setChannels = True, groups = True)
         cntCt = cnt.createController()
         segmentChld.append(cntCt[0])
-        cmds.setAttr (handle + '.visibility', 0)
-        cmds.parentConstraint(cntCt[4],handle, mo=True)
+        cmds.setAttr(handle + '.visibility', 0)
+        cmds.parentConstraint(cntCt[4], handle, mo=True)
         Ctrls.append(cntCt[2])
         misc.cleanUp(cntCt[0], Ctrl=True, SknJnts=False, Body=False, Accessory=False, Utility=False, World=False, olSkool=False)
-        #segment parents
+        # segment parents
         if len(segmentChld) == 4:
-            sgmt = place.Controller('Segment'+ str(('%0' + str(2) + 'd')%(k)), segmentChld[1], orient = False, shape = 'splineEnd_ctrl', size = 100, color = colorP, sections = 8, degree = 1, normal =( 0 , 0 , 1), setChannels = True, groups = True)
+            sgmt = place.Controller('Segment' + str(('%0' + str(2) + 'd') % (k)), segmentChld[1], orient=False, shape='splineEnd_ctrl', size=100, color=colorP, sections=8, degree=1, normal=(0, 0, 1), setChannels = True, groups = True)
             sgmtCt = sgmt.createController()
             misc.scaleUnlock(sgmtCt[2])
             misc.scaleUnlock(sgmtCt[3])
             l = 0
             for child in segmentChld:
                 if l == 3:
-                    cmds.parentConstraint('master_Grp',child, mo=True)
+                    cmds.parentConstraint('master_Grp', child, mo=True)
                 else:
-                    cmds.parentConstraint(sgmtCt[4],child, mo=True)
-                l = l+1
-            cmds.parentConstraint('master_Grp',sgmtCt[0], mo=True)
+                    cmds.parentConstraint(sgmtCt[4], child, mo=True)
+                l = l + 1
+            cmds.parentConstraint('master_Grp', sgmtCt[0], mo=True)
             misc.cleanUp(sgmtCt[0], Ctrl=True, SknJnts=False, Body=False, Accessory=False, Utility=False, World=False, olSkool=False)
             segmentChld = []
-            k=k+1
-        i=i+1
-    i=0
+            k = k + 1
+        i = i + 1
+    i = 0
 
-
-    #cleanup clusters and controllers
+    # cleanup clusters and controllers
     cmds.group(cl, n='Ctrl_Points', w=True)
-    pGrp='Ctrl_Points'
+    pGrp = 'Ctrl_Points'
     misc.cleanUp(pGrp, Ctrl=False, SknJnts=False, Body=False, Accessory=False, Utility=False, World=True, olSkool=False)
 
-    #Make ikSpline for joint chain and assign 'path' as spline-------------------------------------------------------------------------------------------------------------------------------------
+    # Make ikSpline for joint chain and assign 'path' as spline-------------------------------------------------------------------------------------------------------------------------------------
 
     '''
 	ikhandle= cmds.ikHandle( sj= newSpine[0], ee=newSpine[88],sol='ikSplineSolver', ccv=False, c=path, pcv=False)[0]
@@ -184,7 +184,7 @@ def path(*args):
 	cmds.setAttr(ikhandle + '.visibility',0)
 	misc.cleanUp(ikhandle, Ctrl=False, SknJnts=False, Body=False, Accessory=False, Utility=False, World=True, olSkool=False)
 	'''
-    #create twist controls
+    # create twist controls
 
     '''
     cmds.select(Ctrls[0])
@@ -234,7 +234,3 @@ def path(*args):
     cmds.setAttr(MD + '.input2X',0.01)
     cmds.connectAttr(MD + '.outputX', ikhandle + '.offset')
     '''
-
-
-
-
