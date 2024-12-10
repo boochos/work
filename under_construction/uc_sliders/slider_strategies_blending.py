c35ec9cc51226b40183998f65de671df77094106
boochos <boochos@users.noreply.github.com>
2024-12-10 15:54:26 -0500

    got non weighted flattening working

--- under_construction/uc_sliders/slider_strategies_blending.py
+++ under_construction/uc_sliders/slider_strategies_blending.py
@@ -1027,7 +1027,7 @@ class AutoEaseStrategy( AutoTangentStrategy ):
             times_values, base_auto_angle
         )
 
-        return out_angle
+        return in_angle
 
     def _calculate_tangent_angle_flattening( self, times_values, base_auto_angle ):
         """
@@ -1035,29 +1035,32 @@ class AutoEaseStrategy( AutoTangentStrategy ):
         Returns tuple of (in_angle, out_angle)
         """
         try:
+            in_triggered = False
+            out_triggered = False
+
             # Get key values and times
-            current_y = times_values['current_value']
-            prev_y = times_values['prev_value']
-            next_y = times_values['next_value']
+            current_y = times_values['current_value']  # correct
 
-            dt_prev = times_values['current_time'] - times_values['prev_time']
-            dt_next = times_values['next_time'] - times_values['current_time']
+            prev_y = times_values['prev_value']  # correct
+            next_y = times_values['next_value']  # correct
 
-            # print( prev_y, next_y, dt_prev, dt_next )
+            # x distance, correct
+            dt_prev = times_values['current_time'] - times_values['prev_time']  # correct
+            dt_next = times_values['next_time'] - times_values['current_time']  # correct
 
             # Get standard 1/3 distances for control points
-            cp_in_x = dt_prev / 3.0
+            cp_in_x = dt_prev / 3.0 * -1  # flip to negative for in, accomodates get_cp_y method
             cp_out_x = dt_next / 3.0
 
             def get_cp_y( angle, cp_x ):
-                """Calculate control point y position given angle and x offset"""
+                """In tangent is assumed to flipped to negative"""
                 rad = math.radians( angle )
                 dy = math.tan( rad ) * cp_x
-                return current_y - dy  # Subtract for in tangent, add for out
+                return current_y + dy
 
             def get_angle_to_y( target_y, cp_x, current_y ):
                 """Calculate angle needed to hit target y value"""
-                dy = current_y - target_y
+                dy = target_y - current_y
                 return math.degrees( math.atan( dy / cp_x ) )
 
             # Check in tangent
@@ -1065,25 +1068,41 @@ class AutoEaseStrategy( AutoTangentStrategy ):
             in_angle = base_auto_angle
 
             # Test if control point crosses prev key's y value
-            if ( ( current_y > prev_y and in_cp_y < prev_y ) or
-                ( current_y < prev_y and in_cp_y > prev_y ) ):
+            if ( ( current_y >= prev_y and in_cp_y < prev_y ) or
+                ( current_y <= prev_y and in_cp_y > prev_y ) ):
                 flat_in_angle = get_angle_to_y( prev_y, cp_in_x, current_y )
-                # Only use flattened angle if it's less extreme than base angle
+                #
                 if abs( flat_in_angle ) < abs( base_auto_angle ):
                     in_angle = flat_in_angle
+                    in_triggered = True
+                else:
+                    # print( 'no - in' )
+                    pass
 
             # Check out tangent similarly
             out_cp_y = get_cp_y( base_auto_angle, cp_out_x )
             out_angle = base_auto_angle
 
-            print( prev_y, in_cp_y, out_cp_y, next_y )
-
-            if ( ( current_y > next_y and out_cp_y > next_y ) or
-                ( current_y < next_y and out_cp_y < next_y ) ):
+            # Test if control point crosses next key's y value
+            if ( ( current_y >= next_y and out_cp_y < next_y ) or
+                ( current_y <= next_y and out_cp_y > next_y ) ):
+                # print( 'qualified out' )
                 flat_out_angle = get_angle_to_y( next_y, cp_out_x, current_y )
+                #
                 if abs( flat_out_angle ) < abs( base_auto_angle ):
                     out_angle = flat_out_angle
+                    out_triggered = True
+                    # print( 'yes' )
+                else:
+                    # print( 'no - out' )
+                    pass
 
+            #
+            if in_triggered:
+                return in_angle, in_angle
+            if out_triggered:
+                return out_angle, out_angle
+            #
             return in_angle, out_angle
 
         except Exception as e:
@@ -1157,8 +1176,8 @@ class TriangleStaggeredBlendStrategy( TriangleDirectBlendStrategy ):
         self.debug = False
 
         # Range portion parameters, possibly make this more automated, less ditance = higher value, more ditance = smaller value
-        self.min_range_portion = 0.75
-        self.max_range_portion = 0.75
+        self.min_range_portion = 0.7
+        self.max_range_portion = 0.7
         # Local overrides for dynamic range calculation
         self._local_min_range_portion = self.min_range_portion
         self._local_max_range_portion = self.max_range_portion
@@ -1173,7 +1192,7 @@ class TriangleStaggeredBlendStrategy( TriangleDirectBlendStrategy ):
 
         # Auto tangent parameters
         self.transition_to_auto_end = 0.025  # Point where we finish blending to auto tangents
-        self.transition_to_target_start = 1.0  # Point where we start blending to target tangents, 1.0 means stay auto
+        self.transition_to_target_start = 0.99  # Point where we start blending to target tangents, 1.0 means stay auto
 
         # Set auto tangent behavior
         # self.auto_tangent_behavior = AutoSmoothStrategy( core )
